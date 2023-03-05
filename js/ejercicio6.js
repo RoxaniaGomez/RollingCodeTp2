c382888bb6be1b72bf0418c5e793f24163c00e01
roxania <groxania@gmail.com>
2023-03-05 15:44:41 -0300

    corregi caja ejercicio 6

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -1,16 +1,11 @@
-/*Realiza un script que escriba una pirámide del 1 al 
-30 de la siguiente forma :
-1 
-22 
-333 
-4444 
-55555 
-666666 
-…….*/
-for (let index = 1; index < 31; index++) {
-    for (let j = 0; j < index; j++) {
-        document.write(index);
+function ejer6() {
+    let dibujo = "";
+    for (let index = 1; index < 31; index++) {
+        for (let j = 0; j < index; j++) {
 
+            dibujo += index;
+        }
+        dibujo += "<br>";
     }
-    document.write("<br>");
+    document.getElementById("demo6").innerHTML = (dibujo);
 }
